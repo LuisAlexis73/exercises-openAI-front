3ee677d3aec3755614804f157a07c41b8038cef5
Alexis <colo1990.ag@gmail.com>
2024-07-29 23:32:30 -0300

    Add text to audio functionality

--- src/presentation/pages/text-to-audio/TextToAudioPage.tsx
+++ src/presentation/pages/text-to-audio/TextToAudioPage.tsx
@@ -1,5 +1,105 @@
-import React from "react";
+import { useState } from "react";
+import {
+  GptMessage,
+  MyMessage,
+  TypingLoader,
+  TextMessageBoxSelect,
+  GptMessageAudio,
+} from "../../components";
+import { textToAudioUseCase } from "../../../core/use-cases";
+
+const disclaimer = `## Hola, ¿ Que audio quieres generar el día de hoy?
+* Recuerda que todo el audio es generado por AI.`;
+
+const voices = [
+  { id: "nova", text: "Nova" },
+  { id: "alloy", text: "Alloy" },
+  { id: "echo", text: "Echo" },
+  { id: "fable", text: "Fable" },
+  { id: "onyx", text: "Onyx" },
+  { id: "shimmer", text: "Shimmer" },
+];
+
+interface TextMessage {
+  text: string;
+  isGpt: boolean;
+  type: "text";
+}
+
+interface AudioMessage {
+  text: string;
+  isGpt: boolean;
+  audio: string;
+  type: "audio";
+}
+
+type Message = TextMessage | AudioMessage;
 
 export const TextToAudioPage = () => {
-  return <div>TextToAudioPage</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string, selectedVoice: string) => {
+    setIsLoading(true);
+
+    setMessages((prev) => [
+      ...prev,
+      { text: text, isGpt: false, type: "text" },
+    ]);
+
+    const { ok, message, audioUrl } = await textToAudioUseCase(
+      text,
+      selectedVoice
+    );
+
+    setIsLoading(false);
+
+    if (!ok) {
+      return alert(message);
+    }
+
+    setMessages((prev) => [
+      ...prev,
+      {
+        text: `${selectedVoice} - ${message}`,
+        isGpt: true,
+        audio: audioUrl!,
+        type: "audio",
+      },
+    ]);
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid gird-cols-12 gap-y-2">
+          <GptMessage text={disclaimer} />
+
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              message.type === "audio" ? (
+                <GptMessageAudio
+                  key={index}
+                  text={message.text}
+                  audio={message.audio}
+                />
+              ) : (
+                <MyMessage key={index} text={message.text} />
+              )
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            )
+          )}
+
+          {isLoading && <TypingLoader className="fade-in" />}
+        </div>
+      </div>
+
+      <TextMessageBoxSelect
+        onsubmitMessage={handlePost}
+        placeholder="Escribe aqui lo que deseas convertir en audio"
+        options={voices}
+      />
+    </div>
+  );
 };
